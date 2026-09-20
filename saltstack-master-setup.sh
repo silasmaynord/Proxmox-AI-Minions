4749d756e719bc01c2527462bdcfe3dee7b1ed13
silasmaynord <scmaynord23@gmail.com>
2026-09-19 20:55:44 -0500

    Create saltstack-master-setup.sh

---
+++ saltstack-master-setup.sh
@@ -0,0 +1,5 @@
+# Install the SaltStack Master Minion
+sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://saltproject.io
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://saltproject.io bookworm main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo apt update && sudo apt install salt-master -y
+sudo systemctl enable --now salt-master
