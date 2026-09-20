1d7f02230e3cdea9e7765104ea7a0f6d382540bc
silasmaynord <scmaynord23@gmail.com>
2026-09-20 09:09:14 -0500

    Update saltstack-master-setup.sh

--- minion-master/saltstack-master-setup.sh
+++ minion-master/saltstack-master-setup.sh
@@ -1,5 +1,5 @@
 # Install the SaltStack Master Minion on a Proxmox LXC, Debian 12, as the management node
-sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://saltproject.io
+curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://saltproject.io
 echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://saltproject.io bookworm main" | sudo tee /etc/apt/sources.list.d/salt.list
-sudo apt update && sudo apt install salt-master -y
-sudo systemctl enable --now salt-master
+apt update && sudo apt install salt-master -y
+systemctl enable --now salt-master
